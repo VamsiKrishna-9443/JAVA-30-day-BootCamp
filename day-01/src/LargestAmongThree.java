b0bbc394f2ee548a5ae4eadf0fa0aea67ae87fcd
VamsiKrishna-9443 <vamsikrishna.pedapapa.05@gmail.com>
2026-08-13 13:27:23 +0530

    Day 01 test outputs added

--- day-01/src/LargestAmongThree.java
+++ day-01/src/LargestAmongThree.java
@@ -1,2 +1,30 @@
-public class LargestAmongThree {
+import java.util.*;
+public class LargestAmongThree
+{
+    public static int FindLargest(int a , int b , int c)
+    {
+        if(a  > b)
+        {
+            if( a > c)
+            {
+                return a;
+            }
+        }
+        else if ( b > c)
+        {
+            return  b;
+        }
+        return c;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Three Numbers :");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int largest = FindLargest(a,b,c);
+        System.out.println("Largest number :" + largest);
+    }
 }
