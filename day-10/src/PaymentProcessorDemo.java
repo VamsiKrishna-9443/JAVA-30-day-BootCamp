157e201704f8f0c0a38c3f6c9747c78506d4529e
VamsiKrishna-9443 <vamsikrishna.pedapapa.05@gmail.com>
2026-08-24 10:42:13 +0530

    new Program added

---
+++ day-10/src/PaymentProcessorDemo.java
@@ -0,0 +1,48 @@
+interface PaymentProcessor {
+    void processPayment(double amount);
+}
+
+// UPI implementation
+class UPIProcessor implements PaymentProcessor {
+
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing UPI payment of ₹" + amount);
+    }
+}
+
+// Credit Card implementation
+class CreditCardProcessor implements PaymentProcessor {
+
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing Credit Card payment of ₹" + amount);
+    }
+}
+
+// PayPal implementation
+class PayPalProcessor implements PaymentProcessor {
+
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing PayPal payment of ₹" + amount);
+    }
+}
+
+// Main class
+public class PaymentProcessorDemo {
+
+    public static void main(String[] args) {
+
+        PaymentProcessor payment;
+
+        payment = new UPIProcessor();
+        payment.processPayment(1000);
+
+        payment = new CreditCardProcessor();
+        payment.processPayment(2500);
+
+        payment = new PayPalProcessor();
+        payment.processPayment(5000);
+    }
+}
